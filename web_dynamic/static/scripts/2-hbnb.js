042980e1208fdb8ac55e2de9406d0cdc14d9ff63
Shalom Ogoziem <shalomzymike@gmail.com>
2023-11-03 09:57:51 +0000

    task 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  const amenities = {};
+
+  $('input[type="checkbox"]').on('change', function () {
+    if (this.checked) {
+      amenities[this.dataset.name] = this.dataset.id;
+    } else {
+      delete amenities[this.dataset.name];
+    }
+
+    updateAmenitiesList();
+  });
+  function updateAmenitiesList () {
+    const amenitiesList = Object.keys(amenities).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  }
+
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
